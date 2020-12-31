d449326674b2e54021a85f5d0129792953ab4d5b
Jyotivision <aryaljyoti22.ja@gmail.com>
2020-12-30 22:52:50 -0800

    Sorting and foreach loop

--- array.php
+++ array.php
@@ -10,8 +10,45 @@ $total_value=count($age);//count the total number of values
 echo $total_value;
 echo "<br>";
 $c=count($my_array);
-for ($d=0; $d <=$c ; $d++) { 
-	echo$my_array[$d];
+for ($d=0; $d<=$c; $d++) { 
+	echo $my_array[$d];//dont forget to check error here
+}
+echo'<br>';
+//sorting of arrays
+//asort=sort values from highest to lowest
+asort($my_array);
+asort($age);
+echo'<pre>';
+print_r($my_array);
+print_r($age);
+echo'</pre>';
+//arsort=sort values from lowest to highest
+arsort($my_array);
+arsort($age);
+echo'<pre>';
+print_r($my_array);
+print_r($age);
+echo'</pre>';
+//ksort=sort keys from lowest to highest
+ksort($my_array);
+ksort($age);
+echo'<pre>';
+print_r($my_array);
+print_r($age);
+echo'</pre>';
+//krsort=sort keys from highest to lowest
+ksort($my_array);
+ksort($age);
+echo'<pre>';
+print_r($my_array);
+print_r($age);
+echo'</pre>';
+//for each loop
+foreach ($age as $key => $value) {
+	echo $key;
+	echo ":";
+	echo $value;
+	echo "<br>";
 }
 
 ?>
