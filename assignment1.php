ae5f5b11abee5986f4085aa7f7f2f3efbbe1ab4a
Jyotivision <aryaljyoti22.ja@gmail.com>
2021-01-16 07:29:29 -0800

    assignment 1 and 2 updated

--- assignment1.php
+++ assignment1.php
@@ -1,4 +1,12 @@
 <?php
+/* Write a script which runs once and produce the following;
+
+1.       Displayonly even numbers on each line.
+
+2.       Do not show numbers like 10, 20, 30 and so on.
+
+3.       Do not go beyond 200.*/
+
 for($a=0;$a<=200;$a++){
 	if($a%2==0 && $a%10!=0){
 		echo $a.'<br>';
--- assignment2.php
+++ assignment2.php
@@ -1,4 +1,16 @@
 <?php
+/*1.       Create three different indexed arrays containing 5 to 10 values in each array.
+
+2.       Output each of the arrays using for-loop
+
+3.       Output each of arrays using while-loop
+
+4.       Output each of arrays using do-while-loop.
+
+5.       Create an associative array of greetings (values) and days (keys). 
+Declare a variable and you can keep it equals to any name of the day. 
+Now, whenever the name of the day for variable is changed, your script should display relevant greetings message from the associative array.
+ (You are free to choose anything for your keys and values from greetings or days. It’s up to you.) */
 $ages= array(23,24,25,49,69,50 );
 $myarray=array('Ram','Shyam','Hari','Gita','Sita','Rita');
 $marks=array(100,110,95,99,115,120);
